fac448fe31e7165f171469cde7da2bd8aca88956
jeronimo olivera <omegablak17@gmail.com>
2023-02-01 17:07:47 -0300

    'f'

--- src/utils/handlerDate.js
+++ src/utils/handlerDate.js
@@ -8,6 +8,7 @@ module.exports = {
 
 },
 checkDate: (roomsChecks,check_in,check_out) => {
+   console.log(roomsChecks)
    for(let e of roomsChecks){
      if(!(check_in.getTime() > new Date(e.check_out).getTime() || check_out.getTime() < new Date(e.check_in).getTime())) return false
    }
